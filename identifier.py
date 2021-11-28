ecca81bdc1bf700be6bf46ffb1a87dd81c7a33d2
ADITYA BHASKAR <adityabg89@gmail.com>
2021-11-28 14:33:07 +0530

    new

--- identifier.py
+++ identifier.py
@@ -8,39 +8,26 @@ training_images, testing_images = training_images/255, testing_images/255
 
 class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-for i in range(16):
-    plt.subplot(4, 4, i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.imshow(training_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[training_labels[i][0]])
-
-plt.show()
-
 training_images = training_images[:20000]
 training_labels = training_labels[:20000]
 testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D(2, 2))
-model.add(layers.Conv2D(32, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D(2, 2))
-model.add(layers.Conv2D(32, (3,3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+model = models.load_model('image_classification.model')
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+img = cv.imread("car.jpg")
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-model.fit(training_images, training_labels, epochs=10, validation_data={testing_images, testing_labels})
+plt.imshow(img, cmap=plt.cm.binary)
 
-loss, accuracy = model.evaluate(testing_images, testing_labels)
-print(f'{loss}')
-print(f'{accuracy}')
+prediction = model.predict(np.array([img])/255)
+index = np.argmax(prediction)
+print(f'Prediction is {class_names[index]}')
 
-model.save('image_classification.model')
+
+
+
+
 
 
 
